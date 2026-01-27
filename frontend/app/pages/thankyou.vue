3724092d8fa57c949da9ba8132cfb84169fb8dd0
Daniel Mermelstein <danmermel@gmail.com>
2026-01-27 15:01:47 +0000

    adding form to thank you page
    Signed-off-by: Daniel Mermelstein <danmermel@gmail.com>

--- frontend/app/pages/thankyou.vue
+++ frontend/app/pages/thankyou.vue
@@ -12,15 +12,13 @@
       <v-col cols="6">
         <img src="/assets/farmer.jpg" class="my-3" contain height="200" />
       </v-col>
-
-      <v-spacer></v-spacer>
-
       <v-col class="mb-4">
         <h1 class="text-h3 font-weight-bold mb-3">Thank you for voting!</h1>
 
         <p class="subheading font-weight-regular">
-          Stay tuned for results
-        </p>
+          Stay tuned for results! </p>
+        <v-btn href="https://forms.gle/VVTP6pkSHE83b31n8" target="_blank">What did you think of this quiz?
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
